d841ecedb756d79826181877cc9704ceccbbe160
kmishmael <kmishmael@gmail.com>
2025-10-26 02:15:19 +0300

    Hack the coach

---
+++ Hack the coach!/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "6 3" << endl;
+    cout << "1 3" << endl;
+    cout << "2 3" << endl;
+    cout << "3 4" << endl;
+    cout << "4 5" << endl;
+    cout << "4 6" << endl;
+    return 0;
+}
